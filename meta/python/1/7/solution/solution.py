27463fdc600c43c013618863480e6ecb1bf2604a
Федор <fedor@MacBook-Air-Fedor.local>
2019-07-27 17:04:58 +0300

    fix

--- meta/python/1/7/solution/solution.py
+++ meta/python/1/7/solution/solution.py
@@ -1,4 +1,13 @@
-print('True and True = ' + str(True and True))
-print('False and False = ' + str(False and False))
-print('True and False = ' + str(True and False))
-print('False and True = ' + str(False and True))
+hours_of_sleep = int(input())
+consumed_kilocalories = int(input())
+
+hours_of_sleep = max(hours_of_sleep-8, 0)
+
+consumed_kilocalories = (int(consumed_kilocalories / 100) + (consumed_kilocalories % 100 > 0))*100
+consumed_kilocalories = max(consumed_kilocalories-2600, 0)
+
+print(f'''
+Вам нужно бегать {int(hours_of_sleep*10 + consumed_kilocalories/100*5)} минут.
+Стоять в планке {hours_of_sleep*3} минут.
+Отжаться {int(consumed_kilocalories/100*5)} раз.
+''')
